f718742cd37b48767d983dcf78c78592e4a5279c
chiqrar <chiqrar6461@gmail.com>
2025-04-30 17:08:35 +0500

    tailwind css

---
+++ src/page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../Components/Hero.jsx'
+
+const Home = () => {
+  return (
+    <div className='bg-blue-400 transition-all duration-200 overflow-scroll   h-dvh'>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Home
